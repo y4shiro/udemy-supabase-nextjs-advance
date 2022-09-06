0b0c9269eb64dc608afe3d25260d15164d4c3aa3
y4shiro <y4shironao@gmail.com>
2022-09-06 17:25:59 +0900

    add: Profile と編集中の Profile である EditedProfile の型を定義した。また、Zustand で状態管理を行うため Store 内に State / 更新関数 / 初期化関数を定義した。

--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,30 @@
 import create from 'zustand';
 import { Session } from '@supabase/supabase-js';
 
+import { EditedProfile } from './../types/index';
+
 type State = {
   session: Session | null;
   setSession: (payload: Session | null) => void;
+  editedProfile: EditedProfile;
+  updatedEditedProfile: (payload: EditedProfile) => void;
+  resetEditedProfile: () => void;
 };
 
 const useStore = create<State>((set) => ({
   session: null,
   setSession: (payload) => set({ session: payload }),
+  editedProfile: { username: '', favorites: '', avatar_url: '' },
+  updatedEditedProfile: (payload) =>
+    set({
+      editedProfile: {
+        username: payload.username,
+        favorites: payload.favorites,
+        avatar_url: payload.avatar_url,
+      },
+    }),
+  resetEditedProfile: () =>
+    set({ editedProfile: { username: '', favorites: '', avatar_url: '' } }),
 }));
 
 export default useStore;
---
+++ types/index.ts
@@ -0,0 +1,14 @@
+export type Profile = {
+  id: string | undefined;
+  updated_at: string;
+  created_at: string;
+  username: string | undefined;
+  avatar_url: string | undefined;
+  favorites: string | undefined;
+};
+
+export type EditedProfile = {
+  username: string | undefined;
+  avatar_url: string | undefined;
+  favorites: string | undefined;
+};
