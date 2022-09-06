61616e0a661c1a5ce72e6687251865ce7493abbe
y4shiro <y4shironao@gmail.com>
2022-09-06 18:01:00 +0900

    add: Supabase にて Profile の新規作成や更新を行う useMutateProfile hooks と、React Query / Zustand  で Profile の状態を扱う useQueryProfile hooks を実装

---
+++ hooks/useQueryProfile.ts
@@ -0,0 +1,55 @@
+import { useQuery } from 'react-query';
+
+import { supabase } from '../utils/supabase';
+import useStore from '../store';
+import { Profile } from '../types';
+import { useMutateProfile } from './useMutateProfile';
+
+export const useQueryProfile = () => {
+  const session = useStore((state) => state.session);
+  const editedProfile = useStore((state) => state.editedProfile);
+  const update = useStore((state) => state.updatedEditedProfile);
+  const { createProfileMutation } = useMutateProfile();
+
+  const getProfile = async () => {
+    const { data, error, status } = await supabase
+      .from('profile')
+      .select('*')
+      .eq('id', session?.user?.id)
+      .single();
+
+    // Supabase に Profile が無い場合は、この場で Profile を新規作成
+    if (error && status === 406) {
+      // Supabase に Profile を新規作成
+      createProfileMutation.mutate({
+        id: session?.user?.id,
+        username: session?.user?.email,
+        favorites: '',
+        avatar_url: '',
+      });
+      // Zustand の方の username も更新
+      update({ ...editedProfile, username: session?.user?.email });
+    }
+
+    if (error && status !== 406) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  };
+
+  return useQuery<Profile, Error>({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+    staleTime: Infinity,
+    onSuccess: (data) => {
+      if (data) {
+        update({
+          username: data.username,
+          favorites: data.favorites,
+          avatar_url: data.avatar_url,
+        });
+      }
+    },
+  });
+};
